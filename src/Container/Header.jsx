e7ac6418b23815f3c4761debd7205da85339efaf
Umut Palabiyik <57181469+UmutPalabiyik@users.noreply.github.com>
2021-11-01 03:56:16 +0300

    add animation to burger icon

--- src/Container/Header.jsx
+++ src/Container/Header.jsx
@@ -1,39 +1,46 @@
 import { useState } from 'react';
-import { GiHamburgerMenu } from 'react-icons/gi';
 
 const Header = ({ toggleModalShow }) => {
 
-  const [showList, setShowList ] = useState(false);
+  const [burgerAnimation, setBurgerAnimation] = useState(false)
 
+  // for showing login/sign up modal
   const showModal = () => {
     toggleModalShow();
   };
 
-  const showResponsiveList = () => {
-    setShowList(!showList)
-  }
+  const handleBurgerAnimation = () => {
+      setBurgerAnimation(!burgerAnimation);
+  };
+
+  const burgerListItemAnimation = burgerAnimation ? "header__item--animation" : "";
+  const burgerIconAnimation = burgerAnimation ? "header__burger-line--animation" : "";
 
   return (
     <header className={"header"}>
       <p className="header__logo">mook</p>
-      <ul className={`header__list ${showList ? "header__list--responsive" : ""}`}>
-        <li className="header__item">
+      <ul className={`header__list ${burgerAnimation ? "header__list--responsive" : ""}`}>
+        <li className={`header__item  ${burgerListItemAnimation}`}>
           <a className="header__link" href="/">
             <span className="header__text">About</span>
           </a>
         </li>
-        <li className="header__item">
+        <li className={`header__item  ${burgerListItemAnimation}`}>
           <a className="header__link" href="/">
             <span className="header__text">Contact</span>
           </a>
         </li>
-        <li className="header__item">
+        <li className={`header__item  ${burgerListItemAnimation}`}>
           <span className="header__text header__login" onClick={showModal}>
             Join the team
           </span>
         </li>
       </ul>
-      <GiHamburgerMenu className="header__hamburger" onClick={showResponsiveList}/>
+      <div className={`header__burger ${burgerIconAnimation}`} onClick={handleBurgerAnimation}>
+        <div className={"header__burger-line header__burger-line--1"}></div>
+        <div className={"header__burger-line header__burger-line--2"}></div>
+        <div className={"header__burger-line header__burger-line--3"}></div>
+      </div>
     </header>
   );
 };
