dfbafa673c2947cb8f78afbb51de091852c50e39
Umut Palabiyik <57181469+UmutPalabiyik@users.noreply.github.com>
2021-11-08 03:26:23 +0300

    refactor login cover shadows

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
+import { useState } from 'react'
 import './App.scss';
+import Header from './Components/Header';
 import Home from './Container/Home';
 import "./Styles/main.scss";
 
 
 const App = () => {
 
+  
+  const [showModal, setShowModal] = useState(false);
+  const toggleModalShow = () => {
+      setShowModal(!showModal)
+      console.log("modal case", showModal)
+  }
+
   return(
-      <main className="app">
-        <Home />
+      <div className="app">
+        <Header toggleModalShow={toggleModalShow}/>
+        <Home toggleModalShow={toggleModalShow} showModal={showModal}/>
         
 
-      </main>
+      </div>
   )
 }
 
