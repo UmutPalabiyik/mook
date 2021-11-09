ceff9d5d82ef5548afc29f7e7eed31cd9862716e
Umut Palabiyik <57181469+UmutPalabiyik@users.noreply.github.com>
2021-11-10 01:36:05 +0300

    add color variables

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
-import { useState } from 'react'
-import './App.scss';
-import Header from './Components/Header';
-import Home from './Container/Home';
+import { useState } from "react";
+import "./App.scss";
+import Header from "./Components/Header";
+import Home from "./Container/Home";
 import "./Styles/main.scss";
 
-
 const App = () => {
-
-  
   const [showModal, setShowModal] = useState(false);
   const toggleModalShow = () => {
-      setShowModal(!showModal)
-      console.log("modal case", showModal)
-  }
+    setShowModal(!showModal);
+  };
 
-  return(
-      <div className="app">
-        <Header toggleModalShow={toggleModalShow}/>
-        <Home toggleModalShow={toggleModalShow} showModal={showModal}/>
-        
-
-      </div>
-  )
-}
+  return (
+    <div className="app">
+      <Header toggleModalShow={toggleModalShow} />
+      <main className="main">
+        <Home toggleModalShow={toggleModalShow} showModal={showModal} />
+      </main>
+    </div>
+  );
+};
 
 export default App;
