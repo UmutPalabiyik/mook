f5fccf336a6cc927b4bf5a7cbba081a5a73ef01c
Umut Palabiyik <57181469+UmutPalabiyik@users.noreply.github.com>
2021-11-10 21:18:12 +0300

    add images to home section

--- src/Container/Home.jsx
+++ src/Container/Home.jsx
@@ -4,14 +4,17 @@ import { star_wars_jedi_fallen_order } from "../Utils/Helpers/Images.helpers";
 const Home = () => {
   return (
     <section
-      className="home section " /* style={{ backgroundImage: `url(${codBg})` }} */
+      className="home section" /* style={{ backgroundImage: `url(${codBg})` }} */
     >
       <div className="home__container container grid">
-        <img className="home__img" src={star_wars_jedi_fallen_order} alt="" />
+        <img className="home__img section__img" src={star_wars_jedi_fallen_order} alt="" />
         <div className="home__data">
           <h1 className="home__title">Don't play alone</h1>
-          <p className="home__description">You can find friends to play with together here.</p>
-          
+          <p className="home__description">
+            You can find friends to play with together here. Wolves don't travel
+            alone. Join a team. Enjoy more. You can find friends to play with together here.
+          </p>
+          <a className="home__button" href="/">Join the team</a>
         </div>
       </div>
     </section>
