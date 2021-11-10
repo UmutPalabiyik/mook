6cb0da94a4c501e15875b94e3bfe20da61b3a4ef
Umut Palabiyik <57181469+UmutPalabiyik@users.noreply.github.com>
2021-11-10 10:09:08 +0300

    refactor background color and add home selection

--- src/Container/Home.jsx
+++ src/Container/Home.jsx
@@ -1,9 +1,19 @@
-import { codBg } from "../Utils/Helpers/Backgrounds.helpers";
+/* import { codBg } from "../Utils/Helpers/Images.helpers"; */
+import { star_wars_jedi_fallen_order } from "../Utils/Helpers/Images.helpers";
 
 const Home = () => {
   return (
-    <section className="home" style={{ backgroundImage: `url(${codBg})` }}>
-
+    <section
+      className="home section " /* style={{ backgroundImage: `url(${codBg})` }} */
+    >
+      <div className="home__container container grid">
+        <img className="home__img" src={star_wars_jedi_fallen_order} alt="" />
+        <div className="home__data">
+          <h1 className="home__title">Don't play alone</h1>
+          <p className="home__description">You can find friends to play with together here.</p>
+          
+        </div>
+      </div>
     </section>
   );
 };
---
+++ src/Utils/Helpers/Images.helpers.js
@@ -0,0 +1,9 @@
+import codBg from '../../Assets/cod_bg.jpg';
+import kratosBg from '../../Assets/kratos_bg.jpg';
+import miniKratosBg from '../../Assets/mini_kratos_bg.jpg';
+import miniValhallaBg from '../../Assets/mini_valhalla_bg.jpg';
+import itTakesTwo from '../../Assets/it_takes_two.jpg';
+import star_wars_jedi_fallen_order from '../../Assets/star_wars_jedi_fallen_order.png';
+
+
+export {codBg, kratosBg, miniKratosBg, miniValhallaBg, itTakesTwo, star_wars_jedi_fallen_order};
